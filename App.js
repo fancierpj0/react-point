68535046acf7595bbfb0b73ff3fe14a831357d6b
Cris <nicai@163.com>
2018-07-11 19:57:42 +0800

    在React中使用CSS过渡动画

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import './style.css'
 
 export default class App extends React.Component {
+  state = {
+    show: true
+  };
+
   render() {
     return (
-      <div>
-        hello,ahhh
-      </div>
-    )
+      <React.Fragment>
+        <div className={this.state.show ? 'show' : 'hide'}>hello,ahhh</div>
+        <button onClick={()=>this.setState({show:!this.state.show})}>toggle</button>
+      </React.Fragment>
+    );
   }
 }
--- index.js
+++ index.js
@@ -5,4 +5,4 @@ import App from './App';
 
 import TodoList from './TodoList';
 
-ReactDOM.render(<TodoList/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
