1d6c7243ab17e77d7b7d621da712636b2b719548
Cris <nicai@163.com>
2018-07-11 14:44:13 +0800

    用回调函数的形式使用setState

--- TodoList.js
+++ TodoList.js
@@ -3,14 +3,10 @@ import './style.css';
 import TodoItem from './TodoItem';
 
 export default class TodoList extends React.Component {
-  constructor(props) {
-    //调用父类的构造函数
-    super(props);
-    this.state = {
-      inputValue: ''
-      , list: []
-    };
-  }
+  state = this.state = {
+    inputValue: ''
+    , list: []
+  };
 
   handleInputChange = (ev) => {
     // this.state.inputValue = ev.target.value;
@@ -20,23 +16,42 @@ export default class TodoList extends React.Component {
     //但再输入一个2，并不会变为12，而是2
     //TODO React中的state不推荐我们直接修改，而是推荐拷贝一个副本再去操作
 
-    this.setState({inputValue: ev.target.value});
+    // this.setState({inputValue: ev.target.value});
+
+    //这种方式value必须缓存 不然会报错
+    let value = ev.target.value;
+    this.setState((prevState)=>({inputValue: value}));
   };
 
   handleAdd = (ev) => {
     let timestamp = new Date().getTime();
 
-    this.setState({
-      list: [...this.state.list, {id:timestamp,content:this.state.inputValue}]
+    // this.setState({
+    //   list: [...this.state.list, {id:timestamp,content:this.state.inputValue}]
+    //   , inputValue: ''
+    // });
+
+    this.setState((prevState)=>({
+      list: [...prevState.list, {id:timestamp,content:prevState.inputValue}]
       , inputValue: ''
-    });
+    }));
   };
 
   handleDel = (ev, id) => {
-    this.setState({
-      list: this.state.list.filter((item) => item.id !== id)
-    });
+    // this.setState({
+    //   list: this.state.list.filter((item) => item.id !== id)
+    // });
+
+    this.setState((prevState)=>({
+      list: prevState.list.filter((item) => item.id !== id)
+    }));
   };
+
+  getTodoItems(){
+    return this.state.list.map((item, index) => {
+      return <TodoItem item={item} key={item.id} handleDel={this.handleDel}/>;
+    })
+  }
 
   render() {
     /* render函数返回的内容必须整体被包含在一个大的元素中 */
@@ -62,13 +77,7 @@ export default class TodoList extends React.Component {
         </button>
 
         <ul>
-          {
-            this.state.list.map((item, index) => {
-              return (
-                <TodoItem item={item} key={item.id} handleDel={this.handleDel}/>
-              )
-            })
-          }
+          {this.getTodoItems()}
         </ul>
         {/*</div>*/}
       </React.Fragment>
