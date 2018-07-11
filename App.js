75219405c12cc6107626b098374c2e00193e91f8
Cris <nicai@163.com>
2018-07-11 23:12:18 +0800

    TransitionGroup 使用

--- App.js
+++ App.js
@@ -1,38 +1,51 @@
 import React from 'react';
 import './style.css'
-import {CSSTransition} from 'react-transition-group';
+import {CSSTransition,TransitionGroup} from 'react-transition-group';
 
 export default class App extends React.Component {
   state = {
-    show: true
+    list:[]
+  };
+
+  handleAddItem = () => {
+    this.setState((prevState) => {
+      return {
+        list: [...prevState.list, 'item']
+      };
+    });
   };
 
   render() {
     return (
       <React.Fragment>
-        {/*<div className={this.state.show ? 'show' : 'hide'}>hello,ahhh</div>*/}
+        {
+          //TODO 想让每个生成的item都被Transition包裹
 
-        {/*
-          CSSTransition 的classNames 属性有s!!，是添加的类名的前缀，默认就为fade
-          timeout:动画的持续时间
-          unmountOnExit:退出动画结束后会移除dom（注意并不是display：none）
-          onEntered钩子(入场动画结束后触发) 可以放一个函数 函数接收一个参数 ，为包裹的的元素的dom
+          //emm...还是要给每个都套一层CSSTransition
+          //然后还在最外面，所有item外面，套一层 TransitionGroup
+          //此时的好处在于单个item的 CSSTransition 无需设置in
+        }
 
-          appear 定义页面刷新时（或首次进入一个页面时）执行的动画（会触发onEntered钩子函数）
-          appear 会根据 .fade-active{} .fade-appear-active{} 来执行动画
-        */}
-        <CSSTransition
-          in={this.state.show}
-          timeout={1500}
-          classNames='fade'
-          unmountOnExit
-          onEntered={(el)=>{el.style.color='blue'}}
-          appear={true}
-        >
-          <div>hello</div>
-        </CSSTransition>
+        <TransitionGroup>
+          {
+            this.state.list.map((item,index)=>{
+              return (
+                <CSSTransition
+                  timeout={1500}
+                  classNames='fade'
+                  unmountOnExit
+                  onEntered={(el)=>{el.style.color='blue'}}
+                  appear={true}
+                  key={index}
+                >
+                  <div key={index}>{item}</div>
+                </CSSTransition>
+              )
+            })
+          }
+        </TransitionGroup>
 
-        <button onClick={() => this.setState({show: !this.state.show})}>toggle</button>
+        <button onClick={this.handleAddItem}>toggle</button>
       </React.Fragment>
     );
   }
