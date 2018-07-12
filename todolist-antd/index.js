2c3b3eaaca6a5c8586f12b9ee753d8192439a5d6
Cris <nicai@163.com>
2018-07-12 19:59:01 +0800

    将todo更新为无状态组件

--- todolist-antd/index.js
+++ todolist-antd/index.js
@@ -4,18 +4,42 @@ import {Input, Button, List} from 'antd';
 import {connect} from 'react-redux';
 import actions from './store/actions/todo';
 
-@connect(
+// @connect(
+//   state => state.todo
+//   , actions
+// )
+// export default class TodoList extends React.Component {
+//   componentDidMount() {
+//     this.props.load();
+//   }
+//
+//   render() {
+//     let {change, add, del, inputValue, list} = this.props;
+//
+//     return (
+//       <div style={{marginTop: '10px', marginLeft: '10px'}}>
+//         <Input
+//           placeholder='todo info'
+//           style={{width: '300px', marginRight: '10px'}}
+//           value={inputValue}
+//           onChange={change}
+//         />
+//         <Button type="primary" onClick={add}>提交</Button>
+//         <List
+//           style={{marginTo: '10px', width: '300px'}}
+//           bordered
+//           dataSource={list}
+//           renderItem={item => (<List.Item onClick={() => del(item.id)}>{item.content}</List.Item>)}
+//         />
+//       </div>
+//     )
+//   }
+// }
+
+export default connect(
   state => state.todo
   , actions
-)
-export default class TodoList extends React.Component {
-  componentDidMount() {
-    this.props.load();
-  }
-
-  render() {
-    let {change, add, del, inputValue, list} = this.props;
-
+)(function({change, add, del, inputValue, list}){
     return (
       <div style={{marginTop: '10px', marginLeft: '10px'}}>
         <Input
@@ -34,4 +58,4 @@ export default class TodoList extends React.Component {
       </div>
     )
   }
-}
+)
